f9aff5a0eac08e4d369544c462e8b9ac57ec7d42
Alan Lam <s3436174@student.rmit.edu.au>
2020-04-30 20:07:15 +1000

    Update StdSudokuGrid class
    Added in declaration of variables to be used. A 2d array for the sudoku grid. An arraylist to store the lines read from file. Int variables to track the size of arraylist and dimensions of the 2d array.
    
    Implemented initGrid method. This method reads in a file using BufferedReader and splits the read line accordingly to grid coordinates and value which it then uses to create the sudoku grid in the 2d array.
    
    Implemented toString method. Using two simple for loops to create a string to be printed out.

--- rmitSudoku/grid/StdSudokuGrid.java
+++ rmitSudoku/grid/StdSudokuGrid.java
@@ -4,6 +4,7 @@
 package grid;
 
 import java.io.*;
+import java.util.ArrayList;
 
 
 /**
@@ -18,11 +19,17 @@ import java.io.*;
 public class StdSudokuGrid extends SudokuGrid
 {
     // TODO: Add your own attributes
+    public int[][] sudokuGrid;
+    ArrayList<String> sudokuList;
+    int listSize;
+    int gridDimension;
+
 
     public StdSudokuGrid() {
-        super();
 
+        super();
         // TODO: any necessary initialisation at the constructor
+        sudokuList = new ArrayList<String>();
     } // end of StdSudokuGrid()
 
 
@@ -34,6 +41,45 @@ public class StdSudokuGrid extends SudokuGrid
         throws FileNotFoundException, IOException
     {
         // TODO
+        // Open file and read each line using BufferedReader
+        File file = new File(filename);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        String line = null;
+        // Given that the text file follows the proper structure
+        // Assigns read line to variable line and adds to list
+        while ((line = reader.readLine()) != null) {
+            sudokuList.add(line);
+        }
+
+        reader.close();
+
+        listSize = sudokuList.size();
+        // Obtain size of the input grid - first line of file
+        gridDimension = Integer.parseInt(sudokuList.get(0));
+        // Initialise sudoku grid/array to all zeroes
+        sudokuGrid = new int[gridDimension][gridDimension];
+        for(int i = 0; i != gridDimension; ++i) {
+            for(int j = 0; j != gridDimension; ++j) {
+                sudokuGrid[i][j] = 0;
+            }
+        }
+
+        // Assign values read in from file
+        for (int i = 2; i != listSize; ++i) {
+            // Split string into coords and value
+            String[] temp = sudokuList.get(i).split(" ");
+            // Assign value
+            int value = Integer.parseInt(temp[1]);
+            // Split coords string into two separate values using "," and parse as int
+            String[] coords = temp[0].split(",");
+            int row = Integer.parseInt(coords[0]);
+            int column = Integer.parseInt(coords[1]);
+
+            sudokuGrid[row][column] = value;
+
+        }
+
     } // end of initBoard()
 
 
@@ -48,9 +94,20 @@ public class StdSudokuGrid extends SudokuGrid
     @Override
     public String toString() {
         // TODO
+        String gridLayout = "";
+        for (int i = 0; i != gridDimension; ++i) {
+            for (int j = 0; j != gridDimension; ++j) {
+                gridLayout += sudokuGrid[i][j];
+                if (j != gridDimension-1) {
+                    gridLayout += ",";
+                }
+            }
+            gridLayout += "\n";
+        }
 
         // placeholder
-        return String.valueOf("");
+//        return String.valueOf("");
+        return gridLayout;
     } // end of toString()
 
 
